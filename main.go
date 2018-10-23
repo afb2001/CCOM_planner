a7f7eeca0c3f607b41f7bb6c3a0da4c162226db2
Alex Brown <alexb142857@gmail.com>
2018-10-23 12:29:41 -0400

    Revert "Added a hack to fix the start state at beginning of plan issue"

--- main.go
+++ main.go
@@ -310,11 +310,8 @@ type Plan struct {
 }
 
 func (p *Plan) String() string {
-	s := fmt.Sprintf("plan %d", len(p.states)-1) // - 1 is part of the hack
-	for i, state := range p.states {
-		if i == 0 {
-			continue
-		} // hack to skip start state at beginning of plan
+	s := fmt.Sprintf("plan %d", len(p.states))
+	for _, state := range p.states {
 		s += "\n" + state.String()
 	}
 	return s
@@ -348,6 +345,7 @@ func defaultPlan(start *State) *Plan {
 	return plan
 }
 
+// TODO! -- fix duplicate states when planning for multiple goals
 func makePlan(grid *grid, start *State, path path, o *obstacles) *Plan {
 	printLog("Starting to plan")
 
@@ -373,19 +371,12 @@ func makePlan(grid *grid, start *State, path path, o *obstacles) *Plan {
 				start = goal
 				goal = &path[goalCount]
 			} else {
-				printLog("Done planning")
-				//if p.states[0].IsSamePosition(start) { // doesn't work for some reason??
-				//	p.states = p.states[1:] // remove start state from plan
-				//}
 				return p
 			}
 		}
 	}
 
 	printLog("Done planning")
-	//if p.states[0].IsSamePosition(start) { // doesn't work for some reason??
-	//	p.states = p.states[1:] // remove start state from plan
-	//}
 	return p
 }
 
